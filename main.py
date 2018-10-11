4597d5876a36f079e83e776cd51c13676668c8ea
Adam G <25728719+Electromaster232@users.noreply.github.com>
2018-10-11 16:42:37 -0400

    Add Tilde's to markdown

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def send_prev_msg(json, methods=['GET', 'POST']):
     key = json['key']
     for r in msgs[::-1]:
         json2 = {}
-        json2['message'] = markdown.markdown(r[0])
+        json2['message'] = markdown.markdown(r[0], ['pymdownx.tilde'])
         usr = query("SELECT * FROM users WHERE nickname = %s", [r[1]])
         if usr[0][6] == "yes":
             json2['user_name'] = "<i class='fa fa-gavel'></i> " + r[1]
@@ -166,7 +166,7 @@ def handle_chat(json, methods=['GET', 'POST']):
     else:
          query("INSERT INTO messages (content, author, channel) VALUES (%s,%s,%s);", (content, author, channel))
     content = content.replace("/shrug", " ¯\\\_(ツ)_/¯")
-    json['message'] = markdown.markdown(content)
+    json['message'] = markdown.markdown(content, ['pymdownx.tilde'])
     socketio.emit("chatrecieve", json)
     execbot(content, channel)
 
